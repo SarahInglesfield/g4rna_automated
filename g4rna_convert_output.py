ef4481cff9218b3a9101dfdd69cd2b19e9aa070e
Sarah Inglesfield <inglesfs@babraham.ac.uk>
2022-09-08 15:02:34 +0100

    Edited so end file generation works for parallel file submission

--- g4rna_convert_output.py
+++ g4rna_convert_output.py
@@ -6,41 +6,44 @@ def main():
   file_list = read_options()
   #print(file_list)
   #checks()
-  convert_output(file_list.files)
-  flag_done(file_list.files)
-  print(f"All done!")
+  for file in file_list.files:
+    csv_base_name = convert_output(file)
 
-def flag_done(files):
+    print(csv_base_name + "_S2507I_g4rna_convert_end.txt")
+    flag_done(file_list.files, csv_base_name)
+    #print(f"All done with {file}")
+
+def flag_done(files, csv_name):
   dir = os.path.dirname(files[0])
-  done_path = dir +'/g4rna_convert_done.txt'
+  done_path = csv_name + "_S2507I_g4rna_convert_end.txt"
   pathlib.Path(done_path).touch()
 
-def convert_output(file_list):
-  for file in file_list:
-    header = []
-    #extract the name prior to the .tsv ending
-    csv_name = file[:-4]
-    csv_name = csv_name + ".csv"
-    with open(file, 'r') as input_tsv:
-      with open(csv_name, 'w') as output_csv:
-        for line in input_tsv:
-          line_data = line.strip().split("\t")
-          #Need to store header as this contains correct number of fields
-          if not header: #if header is empty (aka first line) then store this line as header
-              #need to swap header field 2&3
-              line_data[1],line_data[2] = line_data[2],line_data[1]
-              header = line_data
-              print(",".join(header),file= output_csv)
-              continue
+def convert_output(file):
+  header = []
+  #extract the name prior to the .tsv ending
+  csv_base_name = file[:-4]
+  csv_name = csv_base_name + ".csv"
+  with open(file, 'r') as input_tsv:
+    with open(csv_name, 'w') as output_csv:
+      for line in input_tsv:
+        line_data = line.strip().split("\t")
+        #Need to store header as this contains correct number of fields
+        if not header: #if header is empty (aka first line) then store this line as header
+            #need to swap header field 2&3
+            line_data[1],line_data[2] = line_data[2],line_data[1]
+            header = line_data
+            print(",".join(header),file= output_csv)
+            continue
 
-          #Need to remove the first field
-          line_data = line_data[1:]
-        
-          #Now need to swap 2nd and 3rd field
-          line_data[1],line_data[2] = line_data[2],line_data[1]
+        #Need to remove the first field
+        line_data = line_data[1:]
+      
+        #Now need to swap 2nd and 3rd field
+        line_data[1],line_data[2] = line_data[2],line_data[1]
 
-          #Print out to csv file
-          print(",".join(line_data),file= output_csv)
+        #Print out to csv file
+        print(",".join(line_data),file= output_csv)
+  return(csv_base_name)
 
 def read_options(): #command line prompt
     parser = argparse.ArgumentParser(description="""
